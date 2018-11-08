24208cde9f12facb29a52f0057215238cf05932b
Emmy Panken <erpanken@gmail.com>
2018-11-08 23:59:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function shout(string) {
   return string.toUpperCase()
 }
+
+function sayHiToGrandma(otherString){
+  let whishper = "I love you, Grandma"
+  console.log(whisper)
+  return whisper.lowercase
+}
